33feb6943276f1e9fca010488e2b65888802f622
tsigereda-affa <affa16.a@gmail.com>
2018-08-16 18:57:39 -0400

    Initial commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -16,48 +15,87 @@ import java.security.Principal;
 
 @Controller
 public class HomeController {
-
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value="/register", method = RequestMethod.GET)
-    public String showRegistrationPage(Model model){
+    @Autowired
+    TaskRepository taskRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegistrationPage(Model model) {
         model.addAttribute("user", new User());
         return "registration";
     }
-    @RequestMapping(value="/register", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     public String processRegistrationPage(
             @Valid @ModelAttribute("user") User user,
             BindingResult result,
-            Model model){
-
-        model.addAttribute("user", user);
+            Model model) {
+        model.addAttribute("user", user); //here
         if (result.hasErrors()) {
             return "registration";
-        }else {
+        } else {
             userService.saveUser(user);
             model.addAttribute("message",
                     "User Account Successfully Created");
         }
         return "index";
     }
+
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
+
     @RequestMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
+
+    @RequestMapping("/update/{id}")
+    public String userUpdate(@PathVariable("id") long id, Model model) {
+        model.addAttribute("task", taskRepository.findById(id).get());
+        return "task";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id){
+        taskRepository.deleteById(id);
+        return "redirect:/secure";
+    }
+
     @RequestMapping("/secure")
-    public String secure(HttpServletRequest request,
-                         Authentication authentication,
-                         Principal principal){
+    public String searchByName(Model model, HttpServletRequest request, Authentication authentication, Principal principal) {
         Boolean isAdmin = request.isUserInRole("ADMIN");
         Boolean isUser = request.isUserInRole("USER");
         UserDetails userDetails = (UserDetails)
-                                  authentication.getPrincipal();
-        String username =principal.getName();
-        return "secure";
+                authentication.getPrincipal();
+        String username = getUser().getUsername();
+        model.addAttribute("tasks", taskRepository.findByUsername(username));
+        return "list";
+
+    }
+    @GetMapping("/add")
+    public String addTask(Model model){
+        model.addAttribute("task", new Task());
+        return "task";
+    }
+    @PostMapping("/process2")
+    public String processForm( @ModelAttribute Task task, BindingResult result, Model model)
+    {
+        String username = getUser().getUsername();
+        task.setUsername(username);
+        taskRepository.save(task);
+        model.addAttribute("tasks", taskRepository.findByUsername(username));
+        return "list";
+    }
+
+    private User getUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentusername = authentication.getName();
+        User user = userRepository.findByUsername(currentusername);
+        return user;
     }
 }
